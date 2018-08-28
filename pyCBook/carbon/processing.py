03182d54c0f71450cb1dd1ed04c189824d51d53a
xjtang <xjtang@bu.edu>
2018-08-28 14:24:18 -0400

    add scale factor

--- pyCBook/carbon/processing.py
+++ pyCBook/carbon/processing.py
@@ -3,19 +3,20 @@
 import math
 
 
-def get_biomass(para, _class):
+def get_biomass(para, _class, scale_factor):
     """ get biomass value from input parameters
 
     Args:
         para (list): input parameters
         _class (str): land cover class
+        scale_factor (float): scale factor
 
     Returns:
         biomass (float): biomass value
 
     """
     para = para[0]
-    return para[para['id'] == _class][0]['biomass']
+    return para[para['id'] == _class][0]['biomass'] * scale_factor
 
 
 def get_flux(para, _class):
@@ -33,7 +34,7 @@ def get_flux(para, _class):
     return para[para['id'] == _class][0]
 
 
-def run_flux(y1, x1, x2, func, coef):
+def run_flux(y1, x1, x2, func, coef, scale_factor):
     """ calculate fluxes
 
     Args:
@@ -42,11 +43,13 @@ def run_flux(y1, x1, x2, func, coef):
         x2 (float): end time
         func (str): decay function
         coef (list, float): decay function coefs
+        scale_factor (float): scale factor
 
     Returns:
         y2 (float): biomass at x2
 
     """
+    coef = coef * scale_factor
     if x1 == x2:
         return y1
     if func == 'linear':
