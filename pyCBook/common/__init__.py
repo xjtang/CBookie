44125964747f9697f84b83d1028c9f3ea111ede5
xjtang <xjtang@bu.edu>
2018-08-28 14:24:37 -0400

    add new function to plot bookkeeping result

--- pyCBook/common/__init__.py
+++ pyCBook/common/__init__.py
@@ -1,7 +1,7 @@
 """ Modules for common libarary
 """
 from .logger import log
-from .plotting import plot_pools
+from .plotting import plot_pools, plot_book
 from .utility import (date_to_doy, doy_to_date, get_files, show_progress,
                         manage_batch, get_date, get_int, doy_to_ordinal,
                         ordinal_to_doy, select_samples, get_class_string)
@@ -20,5 +20,6 @@ __all__ = [
     'ordinal_to_doy',
     'select_samples',
     'plot_pools',
-    'get_class_string'
+    'get_class_string',
+    'plot_book'
 ]
--- pyCBook/common/plotting.py
+++ pyCBook/common/plotting.py
@@ -8,12 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 
-def plot_pools(records, title='Carbon Pools'):
+def plot_pools(records, title='Carbon Pools', des='NA'):
     """ plot carbon pools
 
     Args:
         records (list): input data
         title (str): plot main title
+        des (str): output file
 
     Returns:
         0: successful
@@ -44,11 +45,47 @@ def plot_pools(records, title='Carbon Pools'):
         plot.plot(x, y, _format, lw=2)
 
     # juice up the plot
-    plot.ylabel('Biomass (Mg C / ha.)')
+    plot.ylabel('Biomass (Mg C)')
     plot.xlabel('Date')
     plot.title(title)
     plot.grid(True)
 
     # done
-    plot.show()
+    if des == 'NA':
+        plot.show()
+    else:
+        plot.savefig(des)
+        plot.clf()
+    return 0
+
+
+def plot_book(book, title='Carbon Pools', des='NA'):
+    """ plot bookkeeping results
+
+    Args:
+        book (ndarray): input data
+        title (str): plot main title
+        des (str): output file
+
+    Returns:
+        0: successful
+
+    """
+    # calculate x
+    x = book['date']//1000 + (book['date'] - (book['date']//1000*1000)) / 365.25
+    # make plot
+    plot.plot(x, book['net'], '--', c='0.5', lw=1)
+    plot.plot(x, book['emission'], 'r-', lw=2)
+    plot.plot(x, book['productivity'], 'g-', lw=2)
+    # juice up the plot
+    plot.ylabel('Biomass (Mg C)')
+    plot.xlabel('Date')
+    plot.title(title)
+    plot.grid(True)
+    # done
+    if des == 'NA':
+        plot.show()
+    else:
+        plot.savefig(des)
+        plot.clf()
     return 0
