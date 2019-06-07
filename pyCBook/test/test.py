0b99d6e698e8631e931560f1ab425aab2e53c55f
xjtang <xjtang@bu.edu>
2019-06-06 20:18:31 -0400

    update test

--- pyCBook/test/test.py
+++ pyCBook/test/test.py
@@ -7,7 +7,7 @@ from ..carbon import *
 from ..common import *
 from ..io import *
 
-from .. import book
+from .. import book, area
 from .. import report as rpt
 from .. import plot as plt
 
@@ -15,12 +15,16 @@ class test:
     """ testing
     """
     wd = '/Users/xjtang/Applications/GitHub/CBookie/'
-    para = os.path.join(wd, 'parameters/Colombia/')
+    para = os.path.join(wd, 'parameters/test/')
+    area = os.path.join(wd, 'pyCBook/test/data/area/')
     input = os.path.join(wd, 'pyCBook/test/data/carbon/inputs/')
     output = os.path.join(wd, 'pyCBook/test/data/carbon/outputs/carbon/')
     report = os.path.join(wd, 'pyCBook/test/data/carbon/outputs/report/')
+    daily = os.path.join(wd, 'pyCBook/test/data/carbon/outputs/daily')
+    annual = os.path.join(wd, 'pyCBook/test/data/carbon/outputs/annual')
     figure = os.path.join(wd, 'pyCBook/test/data/carbon/plots/')
     se_biomass = -1
+    pixel_size = 0.3 * 0.3
 
     def __init__(self):
         self.p = [csv2ndarray(os.path.join(self.para, 'biomass.csv')),
@@ -29,23 +33,32 @@ class test:
         self.f = yatsm2records(os.path.join(self.input, 'yatsm_r1.npz'))
         self.df = yatsm2records(os.path.join(self.input, 'yatsm_r2.npz'))
         self.r = yatsm2records(os.path.join(self.input, 'yatsm_r3.npz'))
-        self.f_c = self.get_carbon(self.f, self.se_biomass)
-        self.df_c = self.get_carbon(self.df, self.se_biomass)
-        self.r_c = self.get_carbon(self.r, self.se_biomass)
+        self.df2 = yatsm2records(os.path.join(self.input, 'yatsm_r4.npz'))
+        self.f_c = self.get_carbon(self.f, self.se_biomass, self.pixel_size)
+        self.df_c = self.get_carbon(self.df, self.se_biomass, self.pixel_size)
+        self.df2_c = self.get_carbon(self.df2, self.se_biomass, self.pixel_size)
+        self.r_c = self.get_carbon(self.r, self.se_biomass, self.pixel_size)
         self.f_p = self.get_pools(self.f_c.pools)
         self.df_p = self.get_pools(self.df_c.pools)
+        self.df2_p = self.get_pools(self.df2_c.pools)
         self.r_p = self.get_pools(self.r_c.pools)
+        self.a = csv2ndarray(os.path.join(self.area, 'input.csv'))
+        self.a_p = self.get_aggregated(self.a)
 
     def read_result(self):
-        self.f2 = yatsm2records(os.path.join(self.output, 'carbon_r1.npz'))
-        self.df2 = yatsm2records(os.path.join(self.output, 'carbon_r2.npz'))
-        self.r2 = yatsm2records(os.path.join(self.output, 'carbon_r3.npz'))
+        self.f_2 = yatsm2records(os.path.join(self.output, 'carbon_r1.npz'))
+        self.df_2 = yatsm2records(os.path.join(self.output, 'carbon_r2.npz'))
+        self.r_2 = yatsm2records(os.path.join(self.output, 'carbon_r3.npz'))
+        self.df2_2 = yatsm2records(os.path.join(self.output, 'carbon_r4.npz'))
 
-    def get_carbon(self, pixel, se_biomass):
-        return(carbon(self.p, pixel, se_biomass))
+    def get_carbon(self, pixel, se_biomass=-1, psize=900):
+        return(carbon(self.p, pixel, se_biomass, psize))
 
     def get_pools(self, pixel):
         return(pools(pixel))
+
+    def get_aggregated(self, data):
+        return(aggregated(self.p, self.a))
 
     def record_carbon(self, pixel, se_biomass, des, overwrite=True):
         carbon = self.get_carbon(pixel, se_biomass)
@@ -61,7 +74,18 @@ class test:
 
     def rerun(self):
         book.book_carbon('yatsm_r*.npz', self.input, self.para, self.output,
-                            'NA', True, True)
+                            'NA', 'NA', True, True)
+        rpt.report_line('carbon_r*.npz', [1990001, 2015365], self.output,
+                        self.daily, 1, True)
+        rpt.report_line('carbon_r*.npz', [1990, 2016], self.output, self.annual,
+                        1, True)
+        rpt.report_sum('report_r*.npz', self.daily, os.path.join(self.report,
+                        'daily.csv'), True, True)
+        rpt.report_sum('report_r*.npz', self.annual, os.path.join(self.report,
+                        'annual.csv'), True, True)
+        area.area_carbon(os.path.join(self.area, 'input.csv'), self.para,
+                            os.path.join(self.area, 'output.csv'), [2001, 2015],
+                            2, True)
         return 0
 
     def plot_all(self):
@@ -69,14 +93,16 @@ class test:
         self.plot(self.f_p, 1, os.path.join(self.figure, 'forest_e.png'))
         self.plot(self.df_p, 0, os.path.join(self.figure, 'deforest.png'))
         self.plot(self.df_p, 1, os.path.join(self.figure, 'deforest_e.png'))
+        self.plot(self.df2_p, 0, os.path.join(self.figure, 'deforest2.png'))
+        self.plot(self.df2_p, 1, os.path.join(self.figure, 'deforest2_e.png'))
         self.plot(self.r_p, 0, os.path.join(self.figure, 'regrow.png'))
         self.plot(self.r_p, 1, os.path.join(self.figure, 'regrow_e.png'))
-        plt.plot_report(os.path.join(self.report, 'report_daily.csv'),
-                        os.path.join(self.figure, 'report_daily.png'))
-        plt.plot_report(os.path.join(self.report, 'report_annual.csv'),
-                        os.path.join(self.figure, 'report_annual_cum.png'))
-        plt.plot_report(os.path.join(self.report, 'report_annual.csv'),
-                        os.path.join(self.figure, 'report_annual.png'), False)
+        plt.plot_report(os.path.join(self.report, 'daily.csv'),
+                        os.path.join(self.figure, 'r_daily_cum.png'))
+        plt.plot_report(os.path.join(self.report, 'annual.csv'),
+                        os.path.join(self.figure, 'r_annual_cum.png'))
+        plt.plot_report(os.path.join(self.report, 'annual.csv'),
+                        os.path.join(self.figure, 'r_annual.png'), False)
         return 0
 
     def plot(self, pixel, _which=0, des='NA'):
@@ -98,28 +124,6 @@ class test:
 
 
 class test2:
-    """ testing
-    """
-    lc_1993 = '/Users/xjtang/Downloads/pycbook/ClassM3_1993-01-01_M3train.tif'
-    lc_1997 = '/Users/xjtang/Downloads/pycbook/ClassM3_1997-01-01_M3train.tif'
-    biomass30 = '/Users/xjtang/Downloads/pycbook/biomass_006059.tif'
-
-    def __init__(self):
-        self.lc93 = image2array(self.lc_1993, 1)
-        self.lc97 = image2array(self.lc_1997, 1)
-        self.biomass = image2array(self.biomass30)
-
-    def hist(self, x, bin=10, title='NA'):
-        plot.hist(x, bin)
-        plot.xlabel('Biomass (Mg C / ha.)')
-        plot.ylabel('Frequency')
-        plot.title(title)
-        plot.grid(True)
-        plot.show()
-        return 0
-
-
-class test3:
     """ testing
     """
     wd = '/Users/xjtang/Applications/GitHub/CBookie/'
